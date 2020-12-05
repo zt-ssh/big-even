d4ea820380ed79fab9c33340a0559945694cd261
zt <425045689@qq.com>
2020-12-05 17:49:44 +0800

    完成登陆验证表单验证

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,83 +1,137 @@
-
-$('.layui-form').submit(function(e){
-    var layer=layui.layer
-    e.preventDefault();
-    var data = $(this).serialize()
-    if(data!=''){
-        $.ajax({
-            url:'http://ajax.frontend.itheima.net/api/login',
-            type:'POST',
-            data:data,
-            success:function(res){
-                if(res.status==0){
-                    layer.msg(res.message)
-                    location.href='./index.html'
-                }
-
-            }
-
-        
-           
-        })
-    }
+$(function(){
     
+
+
+    $('.layui-form').submit(function(e){
+        var form =layui.form
+        var layer = layui.layer
+       
+    
+        //表单验证规则
+        form.verify({
+            name: [/^[\S]{6,8}/,'用户名必须是6-8位字符'],
+            pass:[/^[\S]{6,12}$/,'密码必须是6-12位数字']
+        })
+        e.preventDefault();
+        var data = $(this).serialize()
+        if(data!=''){
+            $.ajax({
+                url:'http://ajax.frontend.itheima.net/api/login',
+                type:'POST',
+                data:data,
+                success:function(res){
+                    if(res.status==0){
+                        
+                        location.href='./index.html'
+                        layer.msg(res.message)
+                    }else{
+
+                        //表单提交后 清空
+                        $('.layui-form')[0].reset()
+                       return layer.msg(res.message)
+                       
+                    
+                    }
+    
+                }
+    
+            
+               
+            })
+        }
+        
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
