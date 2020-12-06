be4e06294f2db2c6c3b6efb7a7065100c52b766b
zt <425045689@qq.com>
2020-12-06 12:24:00 +0800

    优化登录注册模块

--- assets/js/login.js
+++ assets/js/login.js
@@ -27,24 +27,29 @@ $(function () {
 
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    $('.layui-form').submit(function (e) {
-        var form = layui.form
-        var layer = layui.layer
+var form = layui.form
+var layer = layui.layer
 
 
-        //表单验证规则
-        form.verify({
-            name: [/^[\S]{6,8}/, '用户名必须是6-8位字符'],
-            pass: [/^[\S]{6,12}$/, '密码必须是6-12位数字'],
-            // 验证确认密码必须和原有密码一致
-    same: function (value) {
-        // 获取原始密码
-        var pass = $('#registerForm input[name=password]').val()
-        if (pass !== value) {
-          return '两次输入的密码必须一致'
-        }
-      }
-        })
+//表单验证规则
+   form.verify({
+    name: [/^[\S]{6,8}/, '用户名必须是6-8位字符'],
+    pass: [/^[\S]{6,12}$/, '密码必须是6-12位数字'],
+    // 验证确认密码必须和原有密码一致
+same: function (value) {
+// 获取原始密码
+var pass = $('#registerForm input[name=password]').val()
+if (pass !== value) {
+  return '两次输入的密码必须一致'
+}
+}
+})  
+
+$('.layui-form').submit(function (e) {
+        
+
+
+       
         e.preventDefault();
         var data = $(this).serialize()
         if (data != '') {
@@ -76,19 +81,20 @@ $(function () {
             })
         }
 
-    })
+    }) 
     //点击切换登录注册
     
     $('#registerForm').on('submit', function(e) {
         // 1. 阻止默认的提交行为
         e.preventDefault();
-        e.stopPropagation();
+     
         
         // 2. 发起Ajax的POST请求
         var data = {
           username: $('#registerForm [name=username]').val(),
-          password: $('#registerForm[name=password]').val()
+          password: $('#registerForm [name=password]').val()
         }
+        console.log(data);
         $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
           if (res.status !== 0) {
             return layer.msg(res.message)
